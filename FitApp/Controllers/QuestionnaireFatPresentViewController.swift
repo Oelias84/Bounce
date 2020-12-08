7ee927c61c6954178ca6501d4395d20aecd82dd2
Ofir Elias <ofir@bthere-tech.com>
2020-12-08 19:43:05 +0200

    Update QuestionnaireFatPresentViewController, QuestionnaireFatCollectionViewCell

--- FitApp/Controllers/QuestionnaireFatPresentViewController.swift
+++ FitApp/Controllers/QuestionnaireFatPresentViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class QuestionnaireFatPresentViewController: UIViewController {
     
-    private let pickerItems = ["fat", "fat", "fat","fat","fat","fat","fat"]
-    private var cellScale : CGFloat = 0.6
+    private let pickerItems = ["Fat1", "Fat2", "Fat3", "Fat4", "Fat5", "Fat6", "Fat7"]
+    private var cellScale: CGFloat = 0.6
+    private var selectedPercentage: String?
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -22,7 +23,30 @@ class QuestionnaireFatPresentViewController: UIViewController {
     }
     
     @IBAction func nextButtonAction(_ sender: Any) {
-        
+        if let fatPresentage = selectedPercentage {
+            var fatPrecent: Double = 0.0
+            switch fatPresentage {
+            case "Fat1":
+                fatPrecent = 10.0
+            case "Fat2":
+                fatPrecent = 20.0
+            case "Fat3":
+                fatPrecent = 30.0
+            case "Fat4":
+                fatPrecent = 40.0
+            case "Fat5":
+                fatPrecent = 50.0
+            case "Fat6":
+                fatPrecent = 60.0
+            case "Fat7":
+                fatPrecent = 70.0
+            default:
+                break
+            }
+            UserProfile.shared.fatPercentage = fatPrecent
+        } else {
+            return
+        }
     }
 }
 
@@ -31,16 +55,13 @@ extension QuestionnaireFatPresentViewController: UICollectionViewDelegate, UICol
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         pickerItems.count
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.CellId.fanCell, for: indexPath) as! QuestionnaireFatCollectionViewCell
-        cell.layer.cornerRadius = 10
-        cell.image.image = UIImage(named: pickerItems[indexPath.row])
+        cell.fatImageString = pickerItems[indexPath.row]
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+        selectedPercentage = pickerItems[indexPath.row]
     }
 }
-
 
--- FitApp/Views/QuestionnaireFatCollectionViewCell.swift
+++ FitApp/Views/QuestionnaireFatCollectionViewCell.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class QuestionnaireFatCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var image: UIImageView!
+    var fatImageString: String! {
+        didSet {
+            cellImage.image = UIImage(named: fatImageString)
+        }
+    }
+    var selectedFat: Bool?
+    
+    @IBOutlet weak var cellImage: UIImageView!
     
     override func layoutSubviews() {
         superview?.layoutSubviews()
+        layer.cornerRadius = 10
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellImage.image = nil
+    }
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                layer.borderColor = UIColor.blue.cgColor
+                layer.borderWidth = 5
+            } else {
+                layer.borderColor = nil
+                layer.borderWidth = 0
+            }
+        }
+    }
 }
