82d811133d289b373bf797ceb100446d625dbaf4
Ofir Elias <ofir@bthere-tech.com>
2020-12-10 11:55:36 +0200

    Update Project - Add QuestionnaireActivityViewController

---
+++ FitApp/Controllers/QuestionnaireActivityViewController.swift
@@ -0,0 +1,33 @@
+//
+//  QuestionnaireActivityViewController.swift
+//  FitApp
+//
+//  Created by iOS Bthere on 10/12/2020.
+//
+
+import UIKit
+
+class QuestionnaireActivityViewController: UIViewController {
+    
+
+    @IBOutlet weak var kilometresSlider: UISlider!
+    @IBOutlet weak var stepsSlider: UISlider!
+    
+    @IBOutlet weak var kilometresLabel: UILabel!
+    @IBOutlet weak var stepsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func nextButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func kilometresSliderAction(_ sender: UISlider) {
+        
+    }
+    @IBAction func stepsSliderAction(_ sender: UISlider) {
+        
+    }
+}
