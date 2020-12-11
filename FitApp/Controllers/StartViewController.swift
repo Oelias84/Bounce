482c76ab9677b2500ef6fbb5963df8b9557ba0b2
Ofir Elias <O.elias84@gmail.com>
2020-12-11 21:00:02 +0200

    Update Project - add StartViewController and automatic login

---
+++ FitApp/Controllers/StartViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StartViewController.swift
+//  FitApp
+//
+//  Created by Ofir Elias on 11/12/2020.
+//
+
+import UIKit
+import FirebaseAuth
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		if Auth.auth().currentUser != nil {
+			let storyboard = UIStoryboard(name: K.StoryboardName.Home, bundle: nil)
+			let homeVC = storyboard.instantiateViewController(identifier: K.StoryboardNameId.HomeTabBar)
+			
+			homeVC.modalPresentationStyle = .fullScreen
+			self.present(homeVC, animated: true)
+	   }
+    }
+}
