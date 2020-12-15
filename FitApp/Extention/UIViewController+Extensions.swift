0de1cc727b8f5459c9bc5cc1f474e5709b4feaf3
Ofir Elias <ofir@bthere-tech.com>
2020-12-15 17:08:18 +0200

    Update UIViewController+Extensions - Add default alerts

--- FitApp/Extention/UIViewController+Extensions.swift
+++ FitApp/Extention/UIViewController+Extensions.swift
@@ -36,4 +36,37 @@ extension UIViewController {
     @objc func dismissView() {
         view.endEditing(true)
     }
+    
+    //Alerts
+    func presentOkAlert(withTitle title: String? = nil, withMessage message: String, buttonText: String = "אישור", completion: @escaping () -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: buttonText, style: .default){ action in
+            completion()
+        })
+        present(alertController, animated: true)
+    }
+    func presentAlert(withTitle title: String? = nil, withMessage message: String, options: (String)..., completion: @escaping (Int) -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction.init(title: option, style: option == "ביטול" ? .destructive : .default, handler: { _ in
+                completion(index)
+            }))
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    func presentActionSheet(withTitle title: String? = nil, withMessage message: String?, options: (String)..., completion: @escaping (Int) -> Void){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction.init(title: option, style: option == "ביטול" ? .destructive : .default, handler: { _ in
+                completion(index)
+            }))
+        }
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
