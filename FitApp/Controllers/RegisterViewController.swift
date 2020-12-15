359ae59cc6d399f66201ff648e95a5085a36a901
Ofir Elias <ofir@bthere-tech.com>
2020-12-15 17:17:37 +0200

    Update RegisterViewController - Add login functionality, and alerts

--- FitApp/Controllers/RegisterViewController.swift
+++ FitApp/Controllers/RegisterViewController.swift
@@ -9,54 +9,45 @@ import UIKit
 import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-	
-	
-	@IBOutlet weak var emailTextfield: UITextField!
-	@IBOutlet weak var passwordTextfield: UITextField!
-	@IBOutlet weak var confirmPasswordTextfield: UITextField!
-	
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-	}
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		raiseScreenWhenKeyboardAppears()
-		addScreenTappGesture()
-	}
-	
-	override func viewWillDisappear(_ animated: Bool) {
-		super.viewWillDisappear(animated)
-		
-		removeKeyboardListener()
-	}
-	
-	@IBAction func singUpButtonAction(_ sender: Any) {
-		
-		if passwordTextfield.text != confirmPasswordTextfield.text {
-			let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
-			let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-			
-			alertController.addAction(defaultAction)
-			self.present(alertController, animated: true, completion: nil)
-		}
-		else{
-			Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!){ (user, error) in
-				if error == nil {
-					let storyboard = UIStoryboard(name: K.StoryboardName.Home, bundle: nil)
-					let homeVC = storyboard.instantiateViewController(identifier: K.StoryboardNameId.HomeTabBar)
-					
-					homeVC.modalPresentationStyle = .fullScreen
-					self.present(homeVC, animated: true)
-				} else {
-					let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-					let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-					
-					alertController.addAction(defaultAction)
-					self.present(alertController, animated: true, completion: nil)
-				}
-			}
-		}
-	}
+    
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var confirmPasswordTextfield: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        raiseScreenWhenKeyboardAppears()
+        addScreenTappGesture()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        removeKeyboardListener()
+    }
+    
+    @IBAction func singUpButtonAction(_ sender: Any) {
+        
+        if passwordTextfield.text != confirmPasswordTextfield.text {
+            presentOkAlert(withTitle: "סיסמה שגויה", withMessage: "אנא נסי שוב") {}
+        } else {
+            Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!){ (user, error) in
+                if error == nil {
+                    let storyboard = UIStoryboard(name: K.StoryboardName.Home, bundle: nil)
+                    let homeVC = storyboard.instantiateViewController(identifier: K.StoryboardNameId.HomeTabBar)
+                    
+                    homeVC.modalPresentationStyle = .fullScreen
+                    self.present(homeVC, animated: true)
+                } else {
+                    self.presentOkAlert(withTitle: "שגיאה", withMessage: error!.localizedDescription) {}
+                }
+            }
+        }
+    }
 }
