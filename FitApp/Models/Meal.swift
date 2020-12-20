da96f92dc1a1e81d8cce2e2a326aab31d2962c20
Ofir Elias <ofir@bthere-tech.com>
2020-12-20 18:53:34 +0200

    Update Meal - Add date Parameter

--- FitApp/Models/Meal.swift
+++ FitApp/Models/Meal.swift
@@ -9,12 +9,14 @@ import Foundation
 
 class Meal {
     
+    let date: Date
     var name: String
     var dishes: [Dish]
     var mealDescription: String?
     var isMealDone: Bool
     
     init(name: String, dishes: [Dish]) {
+        self.date = Date()
         self.name = name
         self.dishes = dishes
         self.isMealDone = false
