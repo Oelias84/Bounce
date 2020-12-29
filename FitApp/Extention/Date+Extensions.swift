acfe248d85daf9890d555f334200e7b378aae49d
Ofir Elias <ofir@bthere-tech.com>
2020-12-29 17:40:19 +0200

    Update Date+Extensions - Add startOfWeek, endOfWeek, displayDayInMonth

--- FitApp/Extention/Date+Extensions.swift
+++ FitApp/Extention/Date+Extensions.swift
@@ -36,14 +36,57 @@ extension Date {
     
     var dateStringDisplay: String {
         let formatter             = DateFormatter()
-        formatter.dateFormat     = "dd/MM/yyyy"
+        formatter.dateFormat     = "dd/MM/yy"
         formatter.locale         = Locale(identifier: "en_Us")
         formatter.timeZone         = .current
         return formatter.string(from: self)
     }
+    
+    var displayMonth: String {
+        let formatter             = DateFormatter()
+        formatter.dateFormat     = "MM/yyyy"
+        formatter.locale         = Locale(identifier: "en_Us")
+        formatter.timeZone         = .current
+        return formatter.string(from: self)
+    }
+    var displayDayInMonth: String {
+        let formatter             = DateFormatter()
+        formatter.dateFormat     = "dd/MM"
+        formatter.locale         = Locale(identifier: "en_Us")
+        formatter.timeZone         = .current
+        return formatter.string(from: self)
+    }
+    var displayDay: String {
+        let formatter             = DateFormatter()
+        formatter.dateFormat     = "dd"
+        formatter.locale         = Locale(identifier: "en_Us")
+        formatter.timeZone         = .current
+        return formatter.string(from: self)
+    }
+    
+    
+    
+    
+    
+    
 	var age: String? {
 		let calendar = Calendar.current
 		let ageComponents = calendar.dateComponents([.year], from: self, to: Date())
 		return "\(ageComponents.year!)"
 	}
+}
+
+extension Date {
+    
+    var startOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+        return gregorian.date(byAdding: .day, value: 0, to: sunday)
+    }
+    
+    var endOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+        return gregorian.date(byAdding: .day, value: 6, to: sunday)
+    }
 }
