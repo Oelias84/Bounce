4de3888e5328d6f0caa3f7d71eb186b336fad160
Ofir Elias <ofir@bthere-tech.com>
2021-01-07 18:05:38 +0200

    Update Project - Add SpinnerView, GoogleApiManager

---
+++ FitApp/Helpers/GoogleApiManager.swift
@@ -0,0 +1,51 @@
+//
+//  GoogleApiManager.swift
+//  FitApp
+//
+//  Created by iOS Bthere on 06/01/2021.
+//
+
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Foundation
+
+struct GoogleApiManager {
+    
+    let db = Firestore.firestore()
+
+    func createDailyMeal(meals: [Meal]) {
+        let currentDate = Date().dateStringForDB
+        let dailyMeals = DailyMeal(meals: meals)
+        do {
+            try db.collection("users").document("\(Auth.auth().currentUser!.uid)").collection("daily-meals").document("\(currentDate)").setData(from: dailyMeals)
+        } catch {
+            print(error)
+        }
+    }
+    func updateMealBy(date: Date, dailyMeal: DailyMeal) {
+        do {
+            try db.collection("users").document("\(Auth.auth().currentUser!.uid)").collection("daily-meals").document("\(date.dateStringForDB)").setData(from: dailyMeal)
+        } catch {
+            print(error)
+        }
+    }
+    func getMealFor( _ date: Date, completion: @escaping (Result<[Meal]?, Error>) -> Void) {
+        db.collection("users").document("\(Auth.auth().currentUser!.uid)").collection("daily-meals").document("\(date.dateStringForDB)")
+            .getDocument(source: .default, completion: { (data, error) in
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                do {
+                    var meals: [Meal]? = nil
+                    let m = try data.data(as: DailyMeal.self)
+                    meals = m?.meals
+                    completion(.success(meals))
+                } catch {
+                    print(error)
+                    completion(.failure(error))
+                }
+            }
+        })
+    }
+}
---
+++ FitApp/Views/SpinnerView.swift
@@ -0,0 +1,29 @@
+//
+//  SpinnerView.swift
+//  FitApp
+//
+//  Created by iOS Bthere on 06/01/2021.
+//
+
+import Foundation
+import UIKit
+
+fileprivate var aView: UIView?
+
+extension UIViewController {
+    
+    func showSpinner() {
+        aView = UIView(frame: self.view.bounds)
+        aView?.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        let ai = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        ai.center = aView!.center
+        ai.startAnimating()
+        aView?.addSubview(ai)
+        self.view.addSubview(aView!)
+    }
+    
+    func stopSpinner() {
+        aView?.removeFromSuperview()
+        aView = nil
+    }
+}
