e681b7b44e9cf08495dc00d776c86f1685beb350
Ofir Elias <ofir@bthere-tech.com>
2021-01-12 17:27:06 +0200

    Update UIView - Add cellView

--- FitApp/Extention/Views/UIView+Extensions.swift
+++ FitApp/Extention/Views/UIView+Extensions.swift
@@ -36,15 +36,6 @@ extension UIView {
 			layer.borderColor = newValue?.cgColor
 		}
 	}
-    func fixInView(_ container: UIView!) -> Void{
-        self.translatesAutoresizingMaskIntoConstraints = false;
-        self.frame = container.frame;
-        container.addSubview(self);
-        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-    }
     
     var parentViewController: UIViewController? {
         var parentResponder: UIResponder? = self
@@ -55,5 +46,22 @@ extension UIView {
             }
         }
         return nil
+    }
+    
+    func fixInView(_ container: UIView!) -> Void {
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+    func cellView() {
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.18
+        layer.shadowColor = UIColor.systemBlue.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 12)
+        layer.shadowRadius = 12
     }
 }
