f0690d15f6bd5d8339cf555d9ba8728a811d08a8
Ofir Elias <ofir@bthere-tech.com>
2021-01-19 15:37:15 +0200

    Update AppDelegate - Add Notification

--- FitApp/AppDelegate.swift
+++ FitApp/AppDelegate.swift
@@ -8,32 +8,71 @@
 
 import UIKit
 import FirebaseCore
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
-		FirebaseApp.configure()
+        
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
 		return true
 	}
 
 	// MARK: UISceneSession Lifecycle
-
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-		// Called when a new scene session is being created.
-		// Use this method to select a configuration to create the new scene with.
+        
 		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
-
 	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-		// Called when the user discards a scene session.
-		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
 	}
+}
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
+    {
+        let id = response.notification.request.identifier
+        print("Received notification with ID = \(id)")
+        
+        if response.notification.request.content.categoryIdentifier == "REMAINDER_INVITATION" {
+            
+            switch response.actionIdentifier {
+            case "ACCEPT_ACTION":
+                
+                guard let window = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window else {
+                    return
+                }
+                let storyboard = UIStoryboard(name: K.StoryboardName.home, bundle: nil)
+                let nav = storyboard.instantiateViewController(withIdentifier: K.StoryboardNameId.HomeTabBar) as? UITabBarController
+                window.rootViewController = nav
+                window.makeKeyAndVisible()
 
+                break
+            case "DECLINE_ACTION":
+                break
+            case UNNotificationDefaultActionIdentifier,
+                 UNNotificationDismissActionIdentifier:
+                break
+            default:
+                break
+            }
+        }
+
+        
+        completionHandler()
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        let id = notification.request.identifier
+        print("Received notification with ID = \(id)")
+        
+        completionHandler([.sound, .alert])
+    }
+    
+    
+    
 }
 
