99d3c6f5d02ae63f76f0836d91dbe08b99d3ea9c
Ofir Elias <O.elias84@gmail.com>
2021-02-14 18:58:41 +0200

    Update StartViewController - remove the Auth validation logic

--- FitApp/Controllers/StartViewController.swift
+++ FitApp/Controllers/StartViewController.swift
@@ -12,19 +12,5 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-//		presentLogoutAlert()
-		Spinner.shared.show(self.view)
-		if Auth.auth().currentUser != nil {
-            let storyboard = UIStoryboard(name: K.StoryboardName.home, bundle: nil)
-			let homeVC = storyboard.instantiateViewController(identifier: K.ViewControllerId.HomeTabBar)
-
-			homeVC.modalPresentationStyle = .fullScreen
-			self.present(homeVC, animated: true) {
-				Spinner.shared.stop()
-			}
-		} else {
-			Spinner.shared.stop()
-		}
     }
 }
