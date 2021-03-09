c675ae6db91244af7964fb3c0097493f9c517772
Ofir Elias <O.elias84@gmail.com>
2021-03-09 16:24:42 +0200

    Update QuestionnaireSumUpViewController - Add lifeStyle configuration

--- FitApp/Controllers/QuestionnaireSumUpViewController.swift
+++ FitApp/Controllers/QuestionnaireSumUpViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class QuestionnaireSumUpViewController: UIViewController {
 	
     private let manager = GoogleApiManager()
-    private let userData = UserProfile.defaults
+    private var userData = UserProfile.defaults
 
 	@IBOutlet weak var ageLabel: UITextField!
 	@IBOutlet weak var weightLabel: UITextField!
@@ -29,11 +29,19 @@ class QuestionnaireSumUpViewController: UIViewController {
 	@IBAction func nextButtonAction(_ sender: Any) {
 
         UserProfile.defaults.finishOnboarding = true
+		
+		if let steps = userData.steps, let height = userData.height {
+			let stepsLengthForMeter = (Double(height) * 0.413) / 100
+			let stepsForOneKilometer = 1000 / stepsLengthForMeter
+			let Kilometer = Double(steps) / stepsForOneKilometer
+			userData.kilometer = Kilometer
+		}
+		
 		let data = ServerUserData(birthDate: userData.birthDate!.dateStringForDB, email: userData.email!, name: userData.name!,
 								  weight: userData.weight!, height: userData.height!, fatPercentage: userData.fatPercentage!,
 								  steps: userData.steps, kilometer: userData.kilometer, lifeStyle: userData.lifeStyle, mealsPerDay: userData.mealsPerDay!,
 								  mostHungry: userData.mostHungry!, fitnessLevel: userData.fitnessLevel!,
-								  weaklyWorkouts: userData.weaklyWorkouts!, finishOnboarding: true)
+								  weaklyWorkouts: userData.weaklyWorkouts!, finishOnboarding: userData.finishOnboarding!)
         
         manager.updateUserData(userData: data)
 		dismiss(animated: true)
@@ -57,11 +65,20 @@ extension QuestionnaireSumUpViewController {
 		} else if let steps = UserProfile.defaults.steps {
 			activityLevelLabel.text = "\(steps) " + K.Units.steps
 		} else {
-			activityLevelLabel.text = K.Units.unknown
+			if let lifeStyle = UserProfile.defaults.lifeStyle {
+				switch lifeStyle {
+				case 1.2:
+					activityLevelLabel.text = "אורח חיים יושבני"
+				case 1.3:
+					activityLevelLabel.text = "אורח חיים פעיל מתון"
+				case 1.5:
+					activityLevelLabel.text = "אורח חיים פעיל"
+				case 1.6:
+					activityLevelLabel.text = "אורח חיים פעיל מאוד"
+				default:
+					activityLevelLabel.text = K.Units.unknown
+				}
+			}
 		}
 	}
-    
-    func updateUserData() {
-        
-    }
 }
