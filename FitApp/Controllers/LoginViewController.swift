90f0fbe6beed5c8ffc8755b1bab7c20609cbdc0a
Ofir Elias <O.elias84@gmail.com>
2021-05-04 12:13:52 +0300

    Update LoginViewController -  Add Login Error handling

--- FitApp/Controllers/LoginViewController.swift
+++ FitApp/Controllers/LoginViewController.swift
@@ -10,6 +10,14 @@ import FirebaseAuth
 
 class LoginViewController: UIViewController {
 	
+	enum LoginError: Error {
+		
+		case emptyEmail
+		case emptyPassword
+		case invalidEmail
+		case incorrectPasswordLength
+	}
+	
 	@IBOutlet weak var emailTextfield: UITextField!
 	@IBOutlet weak var passwordTextfield: UITextField!
     
@@ -29,31 +37,76 @@ class LoginViewController: UIViewController {
 	
 	@IBAction func signInButtonAction(_ sender: Any) {
         view.endEditing(true)
-        guard let email = emailTextfield.text, let password = passwordTextfield.text else { return }
+		
+		do {
+			try login()
+		} catch LoginError.emptyEmail {
+			presentOkAlert(withTitle: "אופס", withMessage: "נראה ששכחת למלא את כתובת האמייל") {
+				self.emailTextfield.becomeFirstResponder()
+			}
+		} catch LoginError.emptyPassword {
+			presentOkAlert(withTitle: "אופס", withMessage: "נראה ששכחת להזין סיסמא") {
+				self.passwordTextfield.becomeFirstResponder()
+			}
+		} catch LoginError.invalidEmail {
+			presentOkAlert(withTitle: "אופס", withMessage: "נראה שכתובת האמייל שגויה") {
+				self.emailTextfield.becomeFirstResponder()
+			}
+		} catch LoginError.incorrectPasswordLength {
+			presentOkAlert(withTitle: "אופס", withMessage: "אורך הסיסמא חייב להיות בעל 6 תווים") {
+				self.passwordTextfield.becomeFirstResponder()
+			}
+		} catch {
+			print("Something went wrong!")
+		}
+	}
+}
+
+extension LoginViewController {
+	
+	private func login() throws {
+		
+		guard let email = emailTextfield.text else {
+			throw LoginError.emptyEmail
+		}
+
+		guard let password = passwordTextfield.text else {
+			throw LoginError.emptyPassword
+		}
+
+		if !email.isValidEmail {
+			throw LoginError.invalidEmail
+		}
+
+		if password.count != 6 {
+			throw LoginError.incorrectPasswordLength
+		}
+			
 		Spinner.shared.show(self.view)
 
-		Auth.auth().signIn(withEmail: email, password: password) { [weak self] (user, error) in
+		Auth.auth().signIn(withEmail: email, password: password) {
+			[weak self] (user, error) in
 			guard let self = self else { return }
 			
 			if error == nil {
 				self.getUserImageProfileUrl(with: email)
-                self.googleManager.getUserData { result in
+				self.googleManager.getUserData { result in
 
-                    switch result {
-                    case .success(let userData):
+					switch result {
+					case .success(let userData):
 						UserProfile.defaults.email = email
 						if let user = user?.user, let data = userData {
 							UserProfile.defaults.updateUserProfileData(data, id: user.uid)
 						}
-                        let storyboard = UIStoryboard(name: K.StoryboardName.home, bundle: nil)
-                        let homeVC = storyboard.instantiateViewController(identifier: K.ViewControllerId.HomeTabBar)
-                        
-                        homeVC.modalPresentationStyle = .fullScreen
-                        self.present(homeVC, animated: true)
-                    case .failure(let error):
-                        print("Error fetching user data: ", error)
-                    }
-                }
+						let storyboard = UIStoryboard(name: K.StoryboardName.home, bundle: nil)
+						let homeVC = storyboard.instantiateViewController(identifier: K.ViewControllerId.HomeTabBar)
+						
+						homeVC.modalPresentationStyle = .fullScreen
+						self.present(homeVC, animated: true)
+					case .failure(let error):
+						print("Error fetching user data: ", error)
+					}
+				}
 			} else {
 				Spinner.shared.stop()
 
@@ -65,8 +118,7 @@ class LoginViewController: UIViewController {
 			}
 		}
 	}
-	
-	func getUserImageProfileUrl(with email: String) {
+	private func getUserImageProfileUrl(with email: String) {
 		let path = "\(email.safeEmail)_profile_picture.jpeg"
 		GoogleStorageManager.shared.downloadImageURL(from: .profileImage , path: path){ result in
 
