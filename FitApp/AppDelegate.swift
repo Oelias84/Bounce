5c1ab07a11cf0fdab2d12a543013af6244694367
Ofir Elias <O.elias84@gmail.com>
2021-05-07 14:31:13 +0300

    Update AppDelegate - Add notification fuctionality

--- FitApp/AppDelegate.swift
+++ FitApp/AppDelegate.swift
@@ -11,45 +11,56 @@ import FirebaseCore
 import FirebaseMessaging
 import UserNotifications
 
+// MARK: - UISceneSession Lifecycle
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
 		FirebaseApp.configure()
-		FirebaseConfiguration.shared.setLoggerLevel(.min)
 		
-		UNUserNotificationCenter.current().delegate = self
-		let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-		UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+		if #available(iOS 10.0, *) {
+		  // For iOS 10 display notification (sent via APNS)
+		  UNUserNotificationCenter.current().delegate = self
+
+		  let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+		  UNUserNotificationCenter.current().requestAuthorization(
+			options: authOptions,
+			completionHandler: {_, _ in })
+		} else {
+		  let settings: UIUserNotificationSettings =
+		  UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+		  application.registerUserNotificationSettings(settings)
+		}
 		application.registerForRemoteNotifications()
+
 		Messaging.messaging().delegate = self
 		
 		return true
 	}
 	
-	// MARK: UISceneSession Lifecycle
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 		
 		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
 }
 
-//MARK: Notifications
+//MARK: - Notifications
 extension AppDelegate: UNUserNotificationCenterDelegate {
 	
 	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-		
+
 		let id = notification.request.identifier
-		print("Received notification with ID = \(id)")
 		
+		print("Received notification with IDluhh = \(id)")
 		completionHandler([.sound, .alert, .badge])
 	}
 	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
 		
 		let id = response.notification.request.identifier
-		print("Received notification with ID = \(id)")
 		
+		print("Received notification with IDkjhk = \(id)")
+
 		if response.notification.request.content.categoryIdentifier == "REMAINDER_INVITATION" {
 			
 			switch response.actionIdentifier {
@@ -60,14 +71,13 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 				}
 				let storyboard = UIStoryboard(name: K.StoryboardName.home, bundle: nil)
 				let nav = storyboard.instantiateViewController(withIdentifier: K.ViewControllerId.HomeTabBar) as? UITabBarController
+				
 				window.rootViewController = nav
 				window.makeKeyAndVisible()
-				
 				break
 			case "DECLINE_ACTION":
 				break
-			case UNNotificationDefaultActionIdentifier,
-				 UNNotificationDismissActionIdentifier:
+			case UNNotificationDefaultActionIdentifier, UNNotificationDismissActionIdentifier:
 				break
 			default:
 				break
@@ -75,7 +85,6 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 		}
 		completionHandler()
 	}
-	
 	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 		Messaging.messaging().apnsToken = deviceToken
 	}
@@ -85,7 +94,8 @@ extension AppDelegate: MessagingDelegate {
 	
 	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
 		let tokenDict = ["token": fcmToken]
-		NotificationCenter.default.post(name: NSNotification.Name("FCMToken"), object: nil, userInfo: tokenDict)
 		
+		UserProfile.defaults.fcmToken = tokenDict["token"]
+		NotificationCenter.default.post(name: NSNotification.Name("FCMToken"), object: nil, userInfo: tokenDict)
 	}
 }
