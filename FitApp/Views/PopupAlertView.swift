8deda5af56e5cf108845699e88a4540c1672369c
Ofir Elias <O.elias84@gmail.com>
2022-01-26 14:23:36 +0200

    Update PopupAlertView - Add TextView for broadcast messages

--- FitApp/Views/PopupAlertView.swift
+++ FitApp/Views/PopupAlertView.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+enum PopupType {
+	
+	case normal
+	case textField
+}
+
 protocol PopupAlertViewDelegate: AnyObject {
 	
 	func okButtonTapped(alertNumber: Int, selectedOption: String?, textFieldValue: String?)
@@ -17,9 +23,13 @@ protocol PopupAlertViewDelegate: AnyObject {
 class PopupAlertView: UIViewController {
 	
 	var alertNumber: Int = 1
+	var popupType: PopupType = .normal
+	
+	@IBOutlet weak var alertView: UIView!
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var messageLabel: UILabel!
-	@IBOutlet weak var alertView: UIView!
+	
+	@IBOutlet weak var textBox: UITextView!
 	@IBOutlet weak var cancelButton: UIButton!
 	@IBOutlet weak var okButton: UIButton!
 	@IBOutlet weak var thirdButton: UIButton!
@@ -39,6 +49,13 @@ class PopupAlertView: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		switch popupType {
+		case .normal:
+			break
+		case .textField:
+			textBox.isHidden = false
+		}
 		
 		if titleText != nil {
 			titleLabel.text = titleText
@@ -69,14 +86,17 @@ class PopupAlertView: UIViewController {
 		super.viewWillAppear(animated)
 		
 		setupView()
+		setupTextView()
 		animateView()
 	}
 	
 	@IBAction func onTapOkButton(_ sender: UIButton) {
-		delegate?.okButtonTapped(alertNumber: alertNumber, selectedOption: nil, textFieldValue: nil)
+		textBox.resignFirstResponder()
+		delegate?.okButtonTapped(alertNumber: alertNumber, selectedOption: nil, textFieldValue: textBox.text)
 		self.dismiss(animated: true, completion: nil)
 	}
 	@IBAction func onTapCancelButton(_ sender: UIButton) {
+		textBox.resignFirstResponder()
 		delegate?.cancelButtonTapped(alertNumber: alertNumber)
 		self.dismiss(animated: true, completion: nil)
 	}
@@ -84,17 +104,45 @@ class PopupAlertView: UIViewController {
 		delegate?.thirdButtonTapped(alertNumber: alertNumber)
 		self.dismiss(animated: true, completion: nil)
 	}
+}
+//MARK: - Delegates
+extension PopupAlertView: UITextViewDelegate  {
+	
+	func textViewDidBeginEditing(_ textView: UITextView) {
+
+		if textBox.textColor == UIColor.lightGray {
+			textBox.text = ""
+			textBox.textColor = .black
+		}
+	}
+	func textViewDidEndEditing(_ textView: UITextView) {
+
+		if textBox.text == "" {
+			textBox.text = "כתבו את תוכן ההודעה…"
+			textBox.textColor = .lightGray
+		}
+	}
+}
+//MARK: - Functions
+extension PopupAlertView {
 	
 	private func setupView() {
 		alertView.layer.cornerRadius = 15
-		self.view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+		view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+	}
+	private func setupTextView() {
+		textBox.delegate = self
+		textBox.text = "כתבו את תוכן ההודעה…"
+		textBox.textColor = .lightGray
 	}
 	private func animateView() {
 		alertView.alpha = 0;
-		self.alertView.frame.origin.y = self.alertView.frame.origin.y + 50
+		alertView.frame.origin.y = self.alertView.frame.origin.y + 50
+		
 		UIView.animate(withDuration: 0.4, animations: { () -> Void in
 			self.alertView.alpha = 1.0;
 			self.alertView.frame.origin.y = self.alertView.frame.origin.y - 50
 		})
 	}
 }
+
