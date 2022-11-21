139691271e8e08e14edd4901d050803607223c7a
Ofir Elias <o.elias84@gmail.com>
2022-11-21 13:53:53 +0200

    Update Project - Add Image to the confetti popup

--- FitApp/Views/CongratsConfettiViewController.swift
+++ FitApp/Views/CongratsConfettiViewController.swift
@@ -12,15 +12,20 @@ class CongratsConfettiViewController: UIViewController {
 
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var animation: LottieAnimationView!
-	
+	@IBOutlet weak var animationTop: LottieAnimationView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+		animationTop.contentMode = .scaleToFill
+		animationTop.loopMode = .loop
+		animationTop.play()
 		animation.contentMode = .scaleToFill
 		animation.loopMode = .loop
 		animation.play()
 		titleLabel.text = StaticStringsManager.shared.getGenderString?[0] ?? "איזה כיף, סיימתי אימון :)"
 	}
+	
 	@IBAction func closeButtonAction(_ sender: Any) {
 		dismiss(animated: true)
 	}
